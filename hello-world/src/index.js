609ca5dab6636c1afb1666e7a6fdb8c5ecd09610
Elliot <elliotgriffiss@gmail.com>
2022-04-13 16:59:23 +0100

    Deleted Hello World Project
    This was done to maintain readability of the repository.

--- hello-world/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'; /* Object: React, Module: 'react'. */
-import ReactDOM from 'react-dom'; /* Object: ReactDOM, Module: 'react-dom'. */
-
-const element = <h1>Hello World</h1>; /* Compiles down to 'React.createElement'; */
-ReactDOM.render(element, document.getElementById('root')); 
-/* Root is the container of our react app as seen in 'public/imdex.html' */
-/* Calling ReactDom.render(element, target); Will place our element inside of the target, we need to use vanila JavaScript to get that target location. */
